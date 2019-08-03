85ca61c8e50be5e0050669d4131517e12522e258
v3nt4n1t0 <48110269+v3nt4n1t0@users.noreply.github.com>
2019-08-03 18:28:12 +0200

    Update DetectNVIDIAVulnDomain.ps1

--- DetectNVIDIAVulnDomain.ps1
+++ DetectNVIDIAVulnDomain.ps1
@@ -24,6 +24,8 @@
 #
 # PS C:\prueba> powershell.exe -ExecutionPolicy Bypass -File 'E:\Pruebas C# PowerShell\DetectNVIDIAVulnDomain.ps1'
 #
+# (Recommended: Save the following command and execute it whenever you want. You do not need to download the script. You will always run the most updated version of the script)
+#
 # PS C:\prueba> iex(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/v3nt4n1t0/DetectNVIDIAVulnDomain.ps1/master/DetectNVIDIAVulnDomain.ps1")
 #
 ################################################################################################################################################## 
